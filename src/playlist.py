28dbcef987832682b7bef404f8987fb658aef1de
Roman Makhnev <romanmahnef777@gmail.com>
2023-12-17 17:26:28 +0400

    Compete task 6

--- src/playlist.py
+++ src/playlist.py
@@ -1,6 +1,5 @@
 import os
 import datetime
-import requests
 from googleapiclient.discovery import build
 
 from src.video import Video
@@ -8,28 +7,50 @@ from src.video import Video
 class PlayList:
     api_key: str = os.getenv('YT_API_KEY')
     youtube = build('youtube', 'v3', developerKey=api_key)
-    def __init__(self, playlist_id: str):
-        self.playlist_id = playlist_id
-        self.title = ""
-        self.url = ""
-        self.videos = [Video(video_id) for video_id in self.video_ids]
 
+    def __init__(self, playlist_id: str) -> None:
+        self.url: str | None = None
+        self.title: str | None = None
+        self.playlist_id: str = playlist_id
         self.get_playlist_info()
 
-    def get_playlist_info(self):
-        api_url = os.getenv('YT_API_KEY')
-        response = requests.get(api_url)
-        data = response.json()
+    def get_playlist_info(self) -> None:
+        """Получаем данные по API и инициализируем ими экземпляр класса."""
+        playlist_info = self.youtube.playlists().list(id=self.playlist_id, part='snippet', ).execute()
+        self.title = playlist_info['items'][0]['snippet']['title']
+        self.url = f'https://www.youtube.com/playlist?list={self.playlist_id}'
 
-        self.title = data["title"]
-        self.url = data["url"]
-        self.videos = data["videos"]
+    def get_playlist_videos(self) -> dict:
+        """Возвращает ответ API на запрос всех видео плейлиста."""
+        playlist_videos = self.youtube.playlistItems().list(playlistId=self.playlist_id,
+                                                            part='contentDetails',
+                                                            maxResults=50,
+                                                            ).execute()
+
+        # получить все id видеороликов из плейлиста
+        video_ids: list[str] = [video['contentDetails']['videoId'] for video in playlist_videos['items']]
+        # получит данные по каждому видео
+        video_response = self.youtube.videos().list(part='contentDetails,statistics',
+                                                    id=','.join(video_ids)
+                                                    ).execute()
+        return video_response
 
     @property
-    def total_duration(self):
-        total_seconds = sum([video["duration"] for video in self.videos])
-        return datetime.timedelta(seconds=total_seconds)
+    def total_duration(self) -> datetime.timedelta:
+        """Возвращает суммарную длительность плей-листа в формате 'datetime.timedelta' (hh:mm:ss))."""
+        video_response = self.get_playlist_videos()
 
-    def show_best_video(self):
-        best_video = max(self.videos, key=lambda x: x["likes"])
-        return best_video["url"]
+        duration = datetime.timedelta()
+        for video in video_response['items']:
+            # Длительности YouTube-видео представлены в ISO 8601 формате
+            iso_8601_duration = video['contentDetails']['duration']
+            duration += isodate.parse_duration(iso_8601_duration)
+
+        return duration
+
+    def show_best_video(self) -> str:
+        """Выводит ссылку на самое залайканое видео в плейлисте."""
+        video_response = self.get_playlist_videos()
+
+        best_video = max([video for video in video_response['items']], key=lambda x: x['statistics']['likeCount'])
+        return f"https://youtu.be/{best_video['id']}"
